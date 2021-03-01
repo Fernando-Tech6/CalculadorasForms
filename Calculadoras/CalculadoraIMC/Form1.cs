59181e2de400bec451e2e61c68ac3b14c2639820
Fernando Vieira <ferdalthor@outlook.com>
2021-03-01 11:35:13 -0300

    Finalizando

--- Calculadoras/CalculadoraIMC/Form1.cs
+++ Calculadoras/CalculadoraIMC/Form1.cs
@@ -17,11 +17,16 @@ namespace CalculadoraIMC
             InitializeComponent();
         }
 
-        private void btnReset_Click(object sender, EventArgs e)
-        {   // Para Limpar todo conteudo do txtBox.
+        private void Resetar()
+        {
+            // Para Limpar todo conteudo do txtBox.
             txtAltura.Text = "";
             txtPeso.Text = "";
             txtImc.Text = "";
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Resetar();
         }
 
         private void btnFechar_Click(object sender, EventArgs e)
@@ -31,48 +36,62 @@ namespace CalculadoraIMC
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
-            // O calculo do IMC é: peso(kg) / quadrado da altura (metros).
-            double peso = Convert.ToDouble(txtPeso.Text);
-            double altura = Convert.ToDouble(txtAltura.Text);
 
-            double Imc = peso / Math.Pow(altura, 2);
+            try
+            {
 
-            txtImc.Text = Imc.ToString("N2");   // resultado sendo exibido no txtbox e com apenas dois digitos apos a virgula.
+                // O calculo do IMC é: peso(kg) / quadrado da altura (metros).
+                double peso = Convert.ToDouble(txtPeso.Text);
+                double altura = Convert.ToDouble(txtAltura.Text);
 
-            //Seguindo a tabela proposta no site.
-            //https://bvsms.saude.gov.br/bvs/dicas/215_obesidade.html
 
-            if (Imc < 18.5)
-            {
-                MessageBox.Show("Você está abaixo do peso", "Mensagem", 
-                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            else if (Imc <= 24.9)
-            {
-                MessageBox.Show("Seu peso está normal", "Mensagem",
-                    MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else if (Imc <= 29.9)
-            {
-                MessageBox.Show("Você está acima do peso", "Mensagem",
-                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-            }
-            else if (Imc <= 34.9)
-            {
-                MessageBox.Show("Acima do peso (Obesidade Grau I)", "Mensagem",
-                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            else if (Imc <= 39.9)
-            {
-                MessageBox.Show("Acima do peso (Obesidade Grau II)", "Mensagem",
-                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            else if (Imc > 40.0)
-            {
-                MessageBox.Show("Acima do peso (Obesidade Grau II)", "Mensagem",
-                    MessageBoxButtons.OK, MessageBoxIcon.Stop);
-            }
+                double Imc = peso / Math.Pow(altura, 2);
 
+                txtImc.Text = Imc.ToString("N2");   // resultado sendo exibido no txtbox e com apenas dois digitos apos a virgula.
+
+                //Seguindo a tabela proposta no site.
+                //https://bvsms.saude.gov.br/bvs/dicas/215_obesidade.html
+
+                if (Imc < 18.5)
+                {
+                    MessageBox.Show("Você está abaixo do peso", "Mensagem",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else if (Imc <= 24.9)
+                {
+                    MessageBox.Show("Seu peso está normal", "Mensagem",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else if (Imc <= 29.9)
+                {
+                    MessageBox.Show("Você está acima do peso", "Mensagem",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else if (Imc <= 34.9)
+                {
+                    MessageBox.Show("Acima do peso (Obesidade Grau I)", "Mensagem",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else if (Imc <= 39.9)
+                {
+                    MessageBox.Show("Acima do peso (Obesidade Grau II)", "Mensagem",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else if (Imc > 40.0)
+                {
+                    MessageBox.Show("Acima do peso (Obesidade Grau II)", "Mensagem",
+                        MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                }
+
+            }
+            catch (Exception )
+            {
+                MessageBox.Show("Digite apenas numeros", "Error", MessageBoxButtons.OK);
+            }
+            finally
+            { 
+                Resetar(); // Apos gerar o erro ou depois do laço de condição ele irá resetar.
+            }
 
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
+# Calculadoras e Formulários !! :computer::computer:
+
+
+
+### Criando algumas calculadoras utilizando windows forms. Será sempre inserido outros tipos  de calculadoras. 
+
+## *Objetivos: Treinando conceitos e criatividade.*
+
+
+
+
+
+
 
